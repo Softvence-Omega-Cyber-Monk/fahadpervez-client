8b1125f3f5835c45fd4b30116951af44d5282d7c
Sohag00879 <shishirsohagislam@gmail.com>
2025-10-03 16:55:00 +0600

    done for today

--- src/pages/Buyer-Dashboard/MyOrders/MyOrders.tsx
+++ src/pages/Buyer-Dashboard/MyOrders/MyOrders.tsx
@@ -1,8 +1,19 @@
+import OrderSearchBar from "../components/MyOrders/OrderSearchbar"
+import OrderTable from "../components/MyOrders/OrderTable"
+import OrderTabs from "../components/MyOrders/OrderTabs"
+import Pagination from "../components/MyOrders/Pagination"
 
 
 const MyOrders = () => {
   return (
-    <div>MyOrders</div>
+     <div>
+      <div>
+      <OrderTabs />
+    </div>
+    <OrderSearchBar/>
+    <OrderTable/>
+    <Pagination/>
+     </div>
   )
 }
 
