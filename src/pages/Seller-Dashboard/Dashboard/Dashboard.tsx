7a58c161e4d691c34be0a8e5e62be26918be49cc
naim0018 <mdkazinaim0018@gmail.com>
2025-10-03 16:39:24 +0600

    Recent order and top products added

--- src/pages/Seller-Dashboard/Dashboard/Dashboard.tsx
+++ src/pages/Seller-Dashboard/Dashboard/Dashboard.tsx
@@ -1,11 +1,19 @@
-
-
+import RecentOrders from "../components/Dashboard/RecentOrders";
+import Stats from "../components/Dashboard/Stats";
+import TopProducts from "../components/Dashboard/TopProducts";
 
 const Dashboard = () => {
-
   return (
-    <div className="">
-      
+    <div className=" space-y-15">
+      <Stats />
+      <div className="flex w-full gap-6 items-start">
+        <div className="flex-2">
+          <RecentOrders />
+        </div>
+        <div className="flex-1">
+          <TopProducts />
+        </div>
+      </div>
     </div>
   );
 };
