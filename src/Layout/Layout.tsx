095fd4915f551af900d32544dd8730545fe74b17
Sohag00879 <shishirsohagislam@gmail.com>
2025-10-03 12:17:54 +0600

    done layout

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -1,11 +1,8 @@
 import Footer from "./Footer";
 import { Outlet } from "react-router-dom";
-import Navbar from "./Navbar";
-
 const Layout: React.FC = () => {
   return (
     <div>
-      <Navbar />
       <main>
         <Outlet />
       </main>
