232cbd8b8690c57bcea9bd33e55f931065da9da6
Sunil Das <sunilpoll1999@gmail.com>
2025-10-03 18:12:33 +0600

    home page completed

--- src/Layout/Navbar.tsx
+++ src/Layout/Navbar.tsx
@@ -7,24 +7,24 @@ const Navbar: React.FC = () => {
 
 
   return (
-    <nav className="mt-8">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-between">
-          {/* Logo */}
-          <div className="flex-shrink-0">
-            <Link to="/" className="text-white text-2xl font-bold">
-              <img src="Logo.png" alt="Logo" />
-            </Link>
-          </div>
+    <nav>
+        <div className="flex justify-center fixed z-50 w-full py-8">
+          <div className="flex items-center justify-between w-[1120px]">
+            {/* Logo */}
+            <div className="flex-shrink-0">
+              <Link to="/" className="text-white text-2xl font-bold">
+                <img src="Logo.png" alt="Logo" />
+              </Link>
+            </div>
 
-          {/* nav icon */}
-           <div className="flex items-baseline gap-4">
-            <Search className="text-[#455058]"/>
-            <ShoppingCart  className="text-[#455058]"/>
-            <CircleUserRound  className="text-[#455058]"/>
-           </div>
-        </div>
-      </div>
+            {/* nav icon */}
+            <div className="flex items-baseline gap-4">
+              <Search className="text-[#455058]"/>
+              <ShoppingCart  className="text-[#455058]"/>
+              <CircleUserRound  className="text-[#455058]"/>
+            </div>
+          </div>
+       </div>
     </nav>
   );
 };
