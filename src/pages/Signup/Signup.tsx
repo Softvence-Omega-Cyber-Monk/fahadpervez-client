d56ffd4038c252b74996ddda31ca29b33176432f
Sunil Das <sunilpoll1999@gmail.com>
2025-10-09 17:56:27 +0600

    basicinfo businessinfo shippinginfo completed

--- src/pages/Signup/Signup.tsx
+++ src/pages/Signup/Signup.tsx
@@ -1,8 +1,20 @@
+import { useState } from "react";
+import BasicInfo from "@/components/AllRegister/BasicInfo/BasicInfo";
+import BusinessInfo from "@/components/AllRegister/BusinessInfo/BusinessInfo";
+import ShippingInfo from "@/components/AllRegister/ShippingInfo/ShippingInfo";
 
 const Signup = () => {
-  return (
-    <div>Signup</div>
-  )
-}
+  const [step, setStep] = useState<"basic" | "business" | "shipping" | "payment">("basic");
+  
 
-export default Signup
+  return (
+    <div>
+      {step === "basic" && <BasicInfo onNext={() => setStep("business")} />}
+      {step === "business" && <BusinessInfo onPrevious={() => setStep("basic")} onNext={() => setStep("shipping")} />}
+      {step === "shipping" && (<ShippingInfo onPrevious={() => setStep("business")} onNext={() => setStep("payment")}/>
+      )}
+    </div>
+  );
+};
+
+export default Signup;
